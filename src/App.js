fc70a10d76e0e2bb4ec999b3dfde46f1430c1270
Ankush Malik <er.ankushmalik@gmail.com>
2023-08-10 13:43:31 +0530

    react-virtualized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { faker } from "@faker-js/faker";
+import { List } from "react-virtualized";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const data = new Array(1000).fill().map((value, id) => ({
+    id: id,
+    title: faker.lorem.words(5),
+    body: faker.lorem.sentences(8),
+  }));
+
+  const renderRow = ({ index, key, style }) => (
+    <div>
+      <div key={key} style={style} className="post">
+        <h3>{`${data[index].title}-${data[index].id}`}</h3>
+        <p>{data[index].body}</p>
+      </div>
     </div>
   );
+  return (
+    <List
+      width={1200}
+      height={700}
+      rowRenderer={renderRow}
+      rowCount={data.length}
+      rowHeight={120}
+    />
+  );
 }
-
 export default App;
